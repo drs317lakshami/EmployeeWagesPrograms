223addd30007438c254ef08f1198fba9a959b187
drs317lakshami <devrajkumar317@gmail.com>
2023-01-04 22:34:54 +0530

    Checking employee is present or absent

--- src/com/bridgelabz/employeewages/EmployeeWages.java
+++ src/com/bridgelabz/employeewages/EmployeeWages.java
@@ -2,7 +2,7 @@ package com.bridgelabz.employeewages;
 
 import java.util.Random;
 
-public class EmployeeWages {
+public class EmployeeWages  {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Java Program");
 
