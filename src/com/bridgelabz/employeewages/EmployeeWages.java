44ef85baaee14c12d0cd64fd6ca7a8ab43b0bc68
drs317lakshami <devrajkumar317@gmail.com>
2023-01-04 23:15:05 +0530

    Calculation of wages using switch cases

--- src/com/bridgelabz/employeewages/EmployeeWages.java
+++ src/com/bridgelabz/employeewages/EmployeeWages.java
@@ -7,32 +7,42 @@ public class EmployeeWages {
 	 * Add Part time Employee & Wage by assuming part time hours is 8
 	 */
 	public static void main(String[] args) {
-		
 		System.out.println("Welcome to Employee Wage Java Program");
 
-		Random random = new Random(); // using random for storing values.
+		Random random = new Random(); // creation of random object
 
+		// initialization
 		int wagePerHrs = 20;
 
 		int fullDayHrs = 8;
 
 		int halfDayHrs = 4;
 
-		int salary = 0; // will give 0,1,2 random number
+		int salary = 0;
+		int randomNum = random.nextInt(3);
 
-		int randomNum = random.nextInt(3); 
+		switch (randomNum) {
 
-		if (randomNum == 1) {
+		case 0:
+			System.out.println("Employee is Absent");
+			System.out.println("Monthly Salary will be: "+salary);
+			break;
+		case 1:
 			System.out.println("Employee is Present");
 			salary = wagePerHrs * fullDayHrs;
-		} else if (randomNum == 2) {
+			System.out.println("Monthly salary for Full Day: " +salary);
+		
+			break;
+		case 2:
+			System.out.println("Employee is Present");
 			salary = wagePerHrs * halfDayHrs;
-			System.out.println("Employee is Present for half day");
+			System.out.println("Monthly Salary for Half Day: "+salary);
+			
+			break;
 
-		} else
-			System.out.println("Employee is Absent");
-		System.out.println("Salary is: " + salary);
+		}
+		
+
 	}
 
 }
-
