412b7c365ad5d3aed5c15f51f5b9761c0a388b95
drs317lakshami <devrajkumar317@gmail.com>
2023-01-04 22:52:24 +0530

    Calculating daily employee wages

--- src/com/bridgelabz/employeewages/EmployeeWages.java
+++ src/com/bridgelabz/employeewages/EmployeeWages.java
@@ -2,17 +2,30 @@ package com.bridgelabz.employeewages;
 
 import java.util.Random;
 
-public class EmployeeWages  {
+/*Calculate Daily
+Employee Wage - Assume Wage per Hour is 20 - Assume Full Day Hour is 8
+*/
+public class EmployeeWages {
 	public static void main(String[] args) {
+		
 		System.out.println("Welcome to Employee Wage Java Program");
 
-		Random random = new Random();
-		int randomNumer = random.nextInt(2);
+		Random random = new Random(); 
 
-		if (randomNumer == 1)
+		int wagePerHrs = 20;
+
+		int fullDayHrs = 8;
+
+		int salary = 0;
+
+		int randomNum = random.nextInt(2);
+
+		if (randomNum == 1) {
 			System.out.println("Employee is Present");
-		else
+			salary = wagePerHrs * fullDayHrs;
+		} else
 			System.out.println("Employee is Absent");
+		System.out.println("Salary is: " + salary);
 	}
 
 }
