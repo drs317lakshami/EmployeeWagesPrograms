b983fa24661e17be1058c9f2ed1a9207c4b407e6
drs317lakshami <devrajkumar317@gmail.com>
2023-01-04 23:02:34 +0530

    Calculation of part time wages

--- src/com/bridgelabz/employeewages/EmployeeWages.java
+++ src/com/bridgelabz/employeewages/EmployeeWages.java
@@ -2,30 +2,37 @@ package com.bridgelabz.employeewages;
 
 import java.util.Random;
 
-/*Calculate Daily
-Employee Wage - Assume Wage per Hour is 20 - Assume Full Day Hour is 8
-*/
 public class EmployeeWages {
+	/*
+	 * Add Part time Employee & Wage by assuming part time hours is 8
+	 */
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Java Program");
 
-		Random random = new Random(); 
+		Random random = new Random(); // using random for storing values.
 
 		int wagePerHrs = 20;
 
 		int fullDayHrs = 8;
 
-		int salary = 0;
+		int halfDayHrs = 4;
 
-		int randomNum = random.nextInt(2);
+		int salary = 0; // will give 0,1,2 random number
+
+		int randomNum = random.nextInt(3); 
 
 		if (randomNum == 1) {
 			System.out.println("Employee is Present");
 			salary = wagePerHrs * fullDayHrs;
+		} else if (randomNum == 2) {
+			salary = wagePerHrs * halfDayHrs;
+			System.out.println("Employee is Present for half day");
+
 		} else
 			System.out.println("Employee is Absent");
 		System.out.println("Salary is: " + salary);
 	}
 
 }
+
