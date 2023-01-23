758b67d16cdf883ddf186bdd6d4f4273931beab8
drs317lakshami <devrajkumar317@gmail.com>
2023-01-23 22:55:10 +0530

    Added: store the daily wage along withh the total wage

--- src/com/bridgelabz/employeewages/IComputeEmpWage.java
+++ src/com/bridgelabz/employeewages/IComputeEmpWage.java
@@ -1,7 +1,6 @@
 package com.bridgelabz.employeewages;
 
 public interface IComputeEmpWage {
-
     public void addCompany(String companyName, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth);
     public void computeEmpWage();
 
