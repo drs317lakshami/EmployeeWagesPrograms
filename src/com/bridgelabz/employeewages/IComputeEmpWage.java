1480b493e3a2148c681e0c59da8c494f31650c1e
drs317lakshami <devrajkumar317@gmail.com>
2023-01-23 22:48:10 +0530

    Added: Refactor to have list of multiple companies to manage employee

--- src/com/bridgelabz/employeewages/IComputeEmpWage.java
+++ src/com/bridgelabz/employeewages/IComputeEmpWage.java
@@ -1,9 +1,9 @@
 package com.bridgelabz.employeewages;
 
 public interface IComputeEmpWage {
+
     public void addCompany(String companyName, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth);
     public void computeEmpWage();
-    public int getTotalEmpWage(String company);
-	
+
 }
 
